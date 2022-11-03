8cc919c238a0066575bef9272d3c3ba986b37a29
toukerahmed <ahamed.touker@gmail.com>
2022-11-03 13:09:57 +0600

    product table editing updated

--- app/Domains/Products/Http/Controllers/ProductController.php
+++ app/Domains/Products/Http/Controllers/ProductController.php
@@ -93,12 +93,7 @@ class ProductController extends Controller
     $updateProduct = product::findOrFail($id);
     if ($updateProduct) {
 
-      // $products = product::pluck('productName', 'id')->toarray();
-      // $products = product::select('productName')->get()->toArray();
-      // $d = product::get(array('productName'));
-      // $dec = json_decode($d);
-      // dd($dec);
-      // dd($products);
+
 
 
       $updateProduct->productName = $request->productName;
