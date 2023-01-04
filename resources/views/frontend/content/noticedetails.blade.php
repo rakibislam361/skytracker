b5fa475aa3e8cfbc608a1bbcb15f49e93d0f9504
Mesbaul-alom <mesbaalam90@gmail.com>
2023-01-04 15:50:40 +0600

    mesba

--- resources/views/frontend/content/noticedetails.blade.php
+++ resources/views/frontend/content/noticedetails.blade.php
@@ -126,104 +126,9 @@
             </div>
         </div> -->
     <!-- preloader end -->
-
-    <!-- header-start -->
     <header>
-        <header>
-
-            <div id="header-sticky" class="main-header">
-                <div class="container-fluid header-container-p">
-                    <div class="row align-items-center">
-                        <div class="col-lg-3 col-md-6">
-                            <div class="logo">
-                                <a href="{{ route('frontend.index') }}"><img
-                                        src="{{ asset(get_setting('frontend_logo_menu')) }}" class="mobile-logo"
-                                        alt="Logo"></a>
-                            </div>
-                        </div>
-                        <div class="col-lg-9 col-md-6 d-none d-md-block">
-                            <div class="menu-area">
-                                <div class="main-menu">
-                                    <div class="mean-push"></div>
-                                    <nav id="mobile-menu" style="display: none;">
-                                        <ul>
-                                            <li class="active"><a href="{{ route('frontend.index') }}">Home</a></li>
-                                            {{-- <li><a href="{{ route('frontend.auth.login') }}">Login</a></li> --}}
-                                            {{-- <li><a href="{{ route('frontend.auth.register') }}">Register</a></li> --}}
-                                            <li> @auth
-                                                    @if ($logged_in_user->isAdmin())
-                                                <li>
-                                                    <a href="{{ route('admin.dashboard') }}">
-                                                        {{ $logged_in_user->name }} </a>
-                                                </li>
-                                                @endif
-
-                                                @if ($logged_in_user->isUser())
-                                                    <li>
-                                                        <a href="{{ route('frontend.user.dashboard') }}">
-                                                            {{ $logged_in_user->name }} </a>
-                                                    </li>
-                                                @endif
-                                            @else
-                                                @if (config('boilerplate.access.user.registration'))
-                                                    <li>
-                                                        <a href="{{ route('frontend.auth.login') }}">Login</a>
-                                                    </li>
-                                                    <li><a href="{{ route('frontend.auth.register') }}">Register</a></li>
-                                                @endif
-
-                                            @endauth
-                                            </li>
-                                            <li><a href="{{ route('frontend.pages.tracking') }}">Tracking</a></li>
-                                            <li><a href="#">Support</a></li>
-
-                                        </ul>
-                                    </nav>
-                                </div>
-
-                                <div class="header-btn">
-                                    <!-- <a href="#" class="btn" data-toggle="modal" data-target="#exampleModalLong"><img src="img/icon/calculator-symbols.png" alt="icon">Get Fare Rate</a> -->
-                                    @auth
-                                        @if ($logged_in_user->isAdmin())
-                                            <li>
-                                                <a href="{{ route('admin.dashboard') }}" class="btn">
-                                                    {{ $logged_in_user->name }} </a>
-                                            </li>
-                                        @endif
-
-                                        @if ($logged_in_user->isUser())
-                                            <li>
-                                                <a href="{{ route('frontend.user.dashboard') }}" class="btn">
-                                                    {{ $logged_in_user->name }} </a>
-                                            </li>
-                                        @endif
-                                    @else
-                                        @if (config('boilerplate.access.user.registration'))
-                                            <li>
-                                                <a href="{{ route('frontend.auth.login') }}"
-                                                    class="btn">Login/Register</a>
-                                            </li>
-                                        @endif
-
-                                    @endauth
-                                    <a href="{{ route('frontend.pages.tracking') }}" class="btn"
-                                        data-target="#exampleModalLong">Track Your
-                                        Order</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="mobile-menu mean-container">
-
-                            </div>
-                        </div>
-                        
-                    </div>
-                </div>
-        </header>
+        @include('frontend.content.header')
     </header>
-
-    <!-- header-start-end -->
 
     <!-- main-area -->
     <main>
@@ -272,7 +177,7 @@
                                                         <a href="/notice/details/{{ $info->id}}"><img src="{{ asset('/setting/banner/'.$info->image) }}" style="height: 30px;width: 40px;" alt="img"></a>
                                                     </div>
                                                     <div class="rc-post-content">
-                                                        <h5><a href="/notice/details/{{ $notice->id}}">{{ $info->title }}</a></h5>
+                                                        <h5><a href="/notice/details/{{ $info->id}}">{{ $info->title }}</a></h5>
                                                         <span>{{ $info->updated_at }}</span>
                                                     </div>
                                                 </li>
@@ -291,134 +196,9 @@
 
     </main>
     <!-- main-area-end -->
-
-    <!-- footer -->
-    <footer>
-        <div class="footer-wrap pt-50" data-background="img/bg/footer_bg.jpg">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-3 col-lg-4 col-md-6">
-                        <div class="footer-widget mb-50">
-                            <div class="footer-logo mb-35">
-                                <a href="{{ route('frontend.index') }}"><img
-                                        src="{{ asset(get_setting('frontend_logo_footer')) }}" alt="img"></a>
-                            </div>
-                            <div class="footer-text">
-                                <p><strong>Head Office</strong></p>
-                                <p>{{ get_setting('office_address') }}</p>
-                                <p><strong>Email</strong></p>
-                                <p>{{ get_setting('office_email') }}</p>
-                                <p><strong>Phone</strong></p>
-                                <p>{{ get_setting('office_phone') }}</p>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-6">
-                        <div class="footer-widget mb-50">
-                            <div class="fw-title mb-30">
-                                <h5>CUSTOMER</h5>
-                            </div>
-                            <div class="fw-link">
-                                <ul>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> About us</a></li>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> Delivery Information</a>
-                                    </li>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> Terms &amp;
-                                            Conditions</a></li>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> Privacy Policy</a></li>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> Refund Policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-6">
-                        <div class="footer-widget mb-50">
-                            <div class="fw-title mb-30">
-                                <h5>INFORMATION</h5>
-                            </div>
-                            <div class="fw-link">
-                                <ul>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> About us</a></li>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> Delivery Information</a>
-                                    </li>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> Terms &amp;
-                                            Conditions</a></li>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> Privacy Policy</a></li>
-                                    <li><a href="#"><i class="fas fa-caret-right"></i> Refund Policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-4 col-md-6">
-                        <div class="footer-widget mb-50">
-                            <div class="fw-title mb-30">
-                                <h5>MOBILE APPS</h5>
-                            </div>
-                            <div class="f-support-content mb-30">
-                                <a href="#" class="f-download-btn"><img
-                                        src="{{ asset('assets/images/images-f_download_btn01.png') }}"
-                                        alt="img"></a>
-                                <a href="#" class="f-download-btn"><img
-                                        src="{{ asset('assets/images/images-f_download_btn02.png') }}"
-                                        alt="img"></a>
-                            </div>
-                            <div class="fw-title mb-30">
-                                <h5>SOCIAL LINKS</h5>
-                            </div>
-                            <div class="f-support-content">
-                                <div class="footer-social">
-                                    <ul>
-                                        <li><a href="{{ get_setting('facebook') }}"><i
-                                                    class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="{{ get_setting('twitter') }}"><i class="fab fa-twitter"></i></a>
-                                        </li>
-                                        <li><a href="{{ get_setting('linkedin') }}"><i
-                                                    class="fab fa-pinterest-p"></i></a></li>
-                                        <li><a href="{{ get_setting('youtube') }}"><i
-                                                    class="fab fa-linkedin-in"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="copyright-wrap">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-12">
-                        <div class="copyright-text">
-                            <p>{{ get_setting('copyright_text') }}</p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </footer>
+   <!-- footer -->
+   @include('frontend.content.footer')
     <!-- footer-end -->
-
-    <script src="{{ asset('assets/js/vendor-jquery-1.12.4.min.js') }}"></script>
-    <script src="{{ asset('assets/js/334-js-popper.min.js') }}"></script>
-    <script src="{{ asset('assets/js/3741-js-bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/5085-js-isotope.pkgd.min.js') }}"></script>
-    <script src="{{ asset('assets/js/3435-js-slick.min.js') }}"></script>
-    <script src="{{ asset('assets/js/312-js-jquery.meanmenu.min.js') }}"></script>
-    <script src="{{ asset('assets/js/9446-js-ajax-form.js') }}"></script>
-    <script src="{{ asset('assets/js/2815-js-wow.min.js') }}"></script>
-    <script src="{{ asset('assets/js/7036-js-aos.js') }}"></script>
-    <script src="{{ asset('assets/js/8726-js-paroller.js') }}"></script>
-    <script src="{{ asset('assets/js/2092-js-jquery.waypoints.min.js') }}"></script>
-    <script src="{{ asset('assets/js/2012-js-jquery.counterup.min.js') }}"></script>
-    <script src="{{ asset('assets/js/2735-js-jquery.nice-select.min.js') }}"></script>
-    <script src="{{ asset('assets/js/7521-js-jquery.scrollUp.min.js') }}"></script>
-    <script src="{{ asset('assets/js/8228-js-imagesloaded.pkgd.min.js') }}"></script>
-    <script src="{{ asset('assets/js/788-js-jquery.magnific-popup.min.js') }}"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCo_pcAdFNbTDCAvMwAD19oRTuEmb9M50c"></script>
-    <script src="{{ asset('assets/js/9626-js-plugins.js') }}"></script>
-    <script src="{{ asset('assets/js/2325-js-main.js') }}"></script>
 
 </body>
 
